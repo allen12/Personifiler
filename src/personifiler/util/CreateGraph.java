c0438ba845a45349787d616cab09b4ba7e23c125
allen12 <computer_allen@yahoo.com>
2015-08-17 15:48:01 -0400

    CreateGraph documentation and javadocs

--- src/personifiler/util/CreateGraph.java
+++ src/personifiler/util/CreateGraph.java
@@ -1,6 +1,5 @@
 package personifiler.util;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -15,28 +14,39 @@ import com.tinkerpop.blueprints.util.io.gml.GMLWriter;
 import com.tinkerpop.blueprints.util.io.graphml.GraphMLWriter;
 
 import personifiler.cluster.ClusterPeople;
-import personifiler.featureMatrix.BinaryFeatureMatrix;
 import personifiler.featureMatrix.FeatureMatrix;
 
 /**
+ * <p> Class that outputs a GML or GraphML file representing a graph.
  * 
- * Class that outputs a .gml file
+ * <p> The resulting graph implementation contains two types of vertices:
+ * clusters and people
+ * 
+ * <p> Edges are drawn from each person vertex to the cluster vertex that they belong to,
+ * according to the results of the Personifiler clustering. Each person vertex also 
+ * contains their actual ground truth group. This allows analytics to be performed on
+ * the graph visualization, such as in {@link http://i.imgur.com/F4kuIap.png}. 
+ * 
+ * <p>The blue nodes are cluster vertices, the red nodes are person vertices. Each person 
+ * node belongs to some colored region, and all the nodes in a colored region belong to the 
+ * same ground truth group. We can then roughly determine how accurate the Personifiler
+ * clustering is based on whether most of the nodes within a colored region have edges
+ * pointing to the same blue cluster node.
  * 
  * @author Allen Cheng
  */
 public class CreateGraph 
 {
-	private Graph graph = new TinkerGraph();
+	private Graph graph;
 	
 	private Map<String, Integer> groups;
-	
-	private FeatureMatrix b;
 	
 	private Vertex[] people;
 	private Vertex[] clusters;
 	private Edge[] edges;
 	private Vertex randIndex;
 	
+	private FeatureMatrix b;
 	private ClusterPeople c;
 	
 	private List<Person> groundTruth = GroundTruth.getList();
@@ -45,6 +55,14 @@ public class CreateGraph
 	{
 		this.b = b;
 		this.c = c;
+	}
+	
+	/**
+	 * Generates a graph, as described in the class documentation.
+	 */
+	public void generateGraph()
+	{
+		graph = new TinkerGraph();
 		
 		addNodes();
 		addClusters();
@@ -59,7 +77,7 @@ public class CreateGraph
 		{
 			int group = groups.get(people[i].getProperty("name"));
 			
-			edges[i] = graph.addEdge(null, people[i], graph.getVertex(group), "lol");
+			edges[i] = graph.addEdge(null, people[i], graph.getVertex(group), "");
 		}
 	}
 	
@@ -114,8 +132,16 @@ public class CreateGraph
 		randIndex.setProperty("rand", Double.toString(r));
 	}
 	
+	/**
+	 * Outputs a GML file
+	 * 
+	 * @param PATH_TO_OUT
+	 */
 	public void createGML(final String PATH_TO_OUT)
 	{
+		if (graph == null)
+			generateGraph();
+		
 		try {
 			GMLWriter.outputGraph(graph, PATH_TO_OUT);
 		} catch (IOException e) {
@@ -123,8 +149,16 @@ public class CreateGraph
 		}
 	}
 	
+	/**
+	 * Outputs a GraphML file
+	 * 
+	 * @param PATH_TO_OUT
+	 */
 	public void createGraphML(final String PATH_TO_OUT)
 	{
+		if (graph == null)
+			generateGraph();
+		
 		try {
 			GraphMLWriter.outputGraph(graph, PATH_TO_OUT);
 		} catch (IOException e) {
